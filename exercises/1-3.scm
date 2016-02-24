55f1fe5fc32de901ed948d9791837e823ab080f6
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-02-24 16:30:13 +0800

    Init Commit

---
+++ exercises/1-3.scm
@@ -0,0 +1,16 @@
+;; ex1.3: Define a procedure that takes three numbers as arguments
+;;  and returns the sum of the squares of the two larger numbers.
+(define (larger-two-sum a b c)
+  (if (= a (larger a b))
+      (sum a (larger b c))
+      (sum b (larger a c))
+  )
+)
+
+(define (larger a b)
+  (if (> a b) a b)
+)
+
+(define (sum a b)
+  (+ a b)
+)
