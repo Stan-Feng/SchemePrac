1a271b95dc52de1fa04a4c5234f36bc0bf67f090
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-02-28 23:45:17 +0800

    Add iterate exponent

--- exercises/1-16.scm
+++ exercises/1-16.scm
@@ -1,20 +1,35 @@
 ;; Calculate power used a seris of square operations
 ;; x--> base, n--> power
-(define (fast-expt x n)
-  (if (even? n)
-    (expt-iter x n)
-    (* x (expt-iter x (- n 1)))
+(define (fast-expt base n)
+  (if (even? base)
+    (expt-iter base n)
+    (* base (expt-iter base (- n 1)))
   )
 )
 
-(define (expt-iter x n)
+(define (expt-iter b n)
   (cond
     ((= n 0) 1)
-    ((= n 1) x)
+    ((= n 1) b)
     (else (*
-            (expt-iter x (/ n 2))
-            (expt-iter x (/ n 2))
+            (expt-iter b (/ n 2))
+            (expt-iter b (/ n 2))
           )
     )
   )
+)
+
+(define (square x)
+  (* x x)
+)
+
+(define (iter-expt base power)
+  (define (iter a base power)
+    (cond
+      ((= power 0) a)
+      ((even? power) (iter a (square base) (/ power 2)))
+      (else (iter (* a base) base (- power 1)))
+    )
+  )
+  (iter 1 base power)
 )
--- js/1-16.js
+++ js/1-16.js
@@ -20,3 +20,26 @@ function exptIter (base, power) {
 }
 
 console.log(expt(5, 2));
+
+function iterExpt (base, power) {
+  var a = 1;
+
+  function _iter (base, power) {
+    if (power === 0) {
+      return a;
+    } else if (power % 2 === 0) {
+      return _iter(square(base), power / 2);
+    } else {
+      a *= base;
+      return _iter(base, power - 1);
+    }
+  }
+
+  return _iter(base, power);
+}
+
+function square (x) {
+  return x * x;
+}
+
+console.log(iterExpt(2, 10));
