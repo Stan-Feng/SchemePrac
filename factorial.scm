107c95b953cbd08ac60db33b5952904b545f2852
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-03-02 19:42:55 +0800

    Wed Mar  2 19:42:55 CST 2016

--- factorial.scm
+++ factorial.scm
@@ -1,9 +1,10 @@
 (define (factorial n)
-  (fact-iter 1 1 n))
+  (define (fact-iter product counter max-count)
+    (if (= max-count counter)
+      (* product counter)
+      (fact-iter (* product counter) (+ counter 1) max-count)
+    )
+  )
 
-(define (fact-iter product counter max-count)
-  (if (> counter max-count)
-    product
-    (fact-iter(* product counter)
-              (+ counter 1)
-              max-count)))
+  (fact-iter 1 1 n)
+)
