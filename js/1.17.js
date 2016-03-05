8f54719077b01428ed0e03bc331a699628808a54
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-03-05 11:22:42 +0800

    Improve 1.17 exercise code

--- js/1.17.js
+++ js/1.17.js
@@ -1,41 +1,45 @@
-'use strict';
+const halve = x => x / 2;
+const double = x => x + x;
 
 function mul (a, b) {
-  var sum = 0;
-  const halve = x => x / 2;
-  const double = x => x + x;
-
-  const iter = (a, b) => {
-    console.log(a, b);
-
-    if (b / 2 === 1) {
-      return sum + double(a);
-    }
-
-    const isEven = (halve(b) % 2 === 0);
-    if (isEven) {
-      sum += double(a);
-      return iter(a, halve(b));
-    } else {
-      sum += double(a) + a;
-      return iter(a, halve(b - 1));
-    }
-
-  };
-
-
-  // Boundaries Situation
   if (b === 0 || a === 0) {
     return 0;
-  } else if (b === 1) {
-    return a;
   }
 
   if (b % 2 === 0) {
-    return iter(a, b);
+    return double(mul(a, halve(b)));
   } else {
-    return a + iter(a, b - 1);
+    return a + mul(a, b - 1);
   }
+  // const iter = (a, b) => {
+  //   // Base Case
+  //   if (b === 2) {
+  //     return double(a);
+  //   } else if (b === 3){
+  //     return a + double(a);
+  //   }
+  //
+  //
+  //   if (b % 2 === 0) {
+  //     return double(iter(a, halve(b)));
+  //   } else {
+  //     return a + double(iter(a, halve(b - 1)));
+  //   }
+  // };
+  //
+  // // Boundaries Situation
+  // if (b === 0 || a === 0) {
+  //   return 0;
+  // } else if (b === 1) {
+  //   return a;
+  // }
+  //
+  // // Enter iter
+  // if (b % 2 === 0) {
+  //   return iter(a, b);
+  // } else {
+  //   return a + iter(a, b - 1);
+  // }
 }
 
 module.exports = mul;
