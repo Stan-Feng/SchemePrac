6e9f4ae374dc02ad3b927d6abf9b604585eef37e
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-03-14 23:20:05 +0800

    Fix bug by looking for solution

--- exercises/2.11.scm
+++ exercises/2.11.scm
@@ -46,40 +46,42 @@
   ; ***************** Helpers from exercise 2.10.scm ******************
 
 (define (mul-interval x y)
-  (define end-sign
-    (lambda (i)
-      (cond
-        ((and (>= (bound-upper i) 0) (>= (lower-bound i) 0)) 1)
-        ((and (<= (bound-upper i) 0) (<= (lower-bound i) 0)) -1)
-        (else 0)
-      )
+  (define (end-sign i)
+    (cond
+      ((and (>= (bound-upper i) 0) (>= (bound-lower i) 0)) 1)
+      ((and (<= (bound-upper i) 0) (<= (bound-lower i) 0)) -1)
+      (else 0)
     )
   )
+
   (let ((es-x (end-sign x))
         (es-y (end-sign y))
         (x-lower (bound-lower x))
         (x-upper (bound-upper x))
         (y-lower (bound-lower y))
         (y-upper (bound-upper y)))
-    (cond
-      (if (and (= es-x 0) (= es-y 0))
-        (make-interval (min (* x-lower y-upper)) (* x-upper y-lower)
-                       (max (* x-lower y-lower) (* x-upper y-upper)))
-        (let ((a1 (if (and (<= es-y 0) (<= (- es-y es-x) 0))
-                    x-upper
-                    y-lower))
-              (a2 (if (and (<= es-x 0) (<= (- es-x es-y) 0))
-                    x-upper
-                    y-lower))
-
-              (b1 (if (and (<= es-y 0) (<= (- es-y es-x) 0))
-                    x-lower
-                    y-upper))
-              (b1 (if (and (<= es-x 0) (<= (- es-x es-y) 0))
-                    x-lower
-                    y-upper)))
-            (make-interval (* a1 a2) (* b1 b2)))
-      )
-    )
+          (if (and (= es-x 0) (= es-y 0))
+            (make-interval (min (* x-lower y-upper) (* x-upper y-lower))
+                           (max (* x-lower y-lower) (* x-upper y-upper)))
+            (let (
+                  (a1 (if (and (<= es-y 0) (<= (- es-y es-x) 0))
+                        x-upper
+                        x-lower))
+                  (a2 (if (and (<= es-x 0) (<= (- es-x es-y) 0))
+                        y-upper
+                        y-lower))
+                  (b1 (if (and (<= es-y 0) (<= (- es-y es-x) 0))
+                        x-lower
+                        x-upper))
+                  (b2 (if (and (<= es-x 0) (<= (- es-x es-y) 0))
+                        y-lower
+                        y-upper))
+                 )
+                (make-interval (* a1 a2) (* b1 b2)))
+          )
   )
 )
+
+;(define x (make-interval 2 4))
+;(define y (make-interval 4 8))
+;(mul-interval x y)
