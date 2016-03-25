204ab7ac410f339115616a8b13006bfb9b30e66c
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-03-25 13:42:08 +0800

    sumPrimePair procedures as modularity

---
+++ js/sumPrimePair.js
@@ -0,0 +1,46 @@
+'use strict';
+// TODO: Given an integer n, find all pairs from 0 to n such that (i, j) where i + j is prime number
+/**
+ * The solving process can be divided into three parts:
+ *    1. Construct Pairs --> [[1, 0], [2, 1], [2, 0].......]
+ *      - Enumerate from 1 to (n - 1) and for each number i map from 0 to (i - 1) return a pair
+ *    2. Filter sum-prime number
+ *    3. Mapping out to final result
+ */
+ function makeSumPrimePair (num) {
+   return flatmap(enumerateInterval(1, num), enumerateInterval)
+     .filter(isSumPrime)
+     .map(el => ({ i: el[0], j: el[1], result: el[0] + el[1]}));
+ }
+
+function enumerateInterval (start, end) {
+  const result = new Array(end - start);
+
+  for (let i = 0, val = start; val <= end; val++, i++) {
+    result[i] = val;
+  }
+
+  return result;
+};
+
+function flatmap (sequence, proc) {
+  return sequence
+    .map(i => (proc (0, i - 1)
+      .map(j => [i, j])))
+    .reduce((accu, curr) => (accu.concat(curr)));
+}
+
+function isSumPrime (pair) {
+  const isPrime = function (x) {
+    for (let i = 2; i < x; i++) {
+      if (x % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  return isPrime(pair[0] + pair[1]);
+}
+
+console.log(makeSumPrimePair(6));
